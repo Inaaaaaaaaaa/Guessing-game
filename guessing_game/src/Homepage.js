bba9c9ab2aa7cc213b815ef235c8154d24614077
Latte123456 <inayumi7@gmail.com>
2024-04-13 19:55:57 +1200

    Button and box changes

--- guessing_game/src/Homepage.js
+++ guessing_game/src/Homepage.js
@@ -5,11 +5,22 @@ function Homepage()
 {
     return (
 
-            <div className='homepage-container'>
+            <div className='homepages'>
                <div className="homepage">
                 <h1>Welcome to the guessing game!</h1>
+
+                <div className='homepage-container'>
+                    <div class="box">
+                    <form>
+                    <input type="text" placeholder='Enter your number' required />
+                    <button type="guess">Guess</button>
+                    </form>
+                    </div>
+                    
+                </div>
                </div>
             </div>
+
     );
 }
 
